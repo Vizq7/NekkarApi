480f6254248c4a48b18487b28aa285cbab837437
Victor Izquierdo <v.izquierdo16@gmail.com>
2019-04-25 22:56:26 +0200

    add sequelize package with connection to db

--- app.js
+++ app.js
@@ -9,6 +9,31 @@ app.use(router.router);
 
 const PORT = 5000;
 
+const Sequelize = require("sequelize")
+var sequelize = new Sequelize("XWingsFactory", "nekkar", "S2am2018",{
+  host: "nekkar.database.windows.net",
+  dialect: "mssql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  dialectOptions: {
+    options: {
+      encrypt: true
+    }
+  }
+})
+
 app.listen(process.env.PORT || 5000, () => {
-  //console.log(`server running on port ${PORT}`)
+  console.log(`server running on port ${PORT}`)
+  
+  sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 });
